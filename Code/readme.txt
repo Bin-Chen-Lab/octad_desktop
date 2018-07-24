b352e568e116f268373cb8fe45638697dd02f95d
bzeng <billy.zeng@ucsf.edu>
2018-07-23 21:14:55 -0400

    Initial Commit
    Adapted from Anita Clone 6 version.

---
+++ Code/readme.txt
@@ -0,0 +1,2 @@
+Codes for Web Portal
+Codes for each results are stored in their respective folders in case of changes
---
+++ Code/set_parameters.R
@@ -0,0 +1,9 @@
+####default settings####
+#keep a record of these settings if you're doing multiple runs
+
+####normalization options####
+normalize_samples = T
+k = 1
+n_topGenes = 10000
+DE_method = 'edgeR' #limma #edgeR
+
